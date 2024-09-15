174ab9096b9edd06b5cf3c36fc32622fbd3897da
Adam Owada <adamowada@gmail.com>
2024-09-15 04:32:30 -0700

    trying 2 functions in split files

---
+++ api/get-questions.py
@@ -0,0 +1,15 @@
+# from aiohttp import web
+from fastapi import FastAPI
+
+from chatgpt.question_generator import generate_questions
+from database.db_operations import insert_question, insert_answer, get_client, get_all_questions_and_answers
+
+
+app = FastAPI()
+
+
+@app.get("/api/get-questions")
+async def get_questions():
+    client = await get_client()
+    questions_data = await get_all_questions_and_answers(client)
+    return questions_data
